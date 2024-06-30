0f89d56a622dd19317b5563d3cccd3602d83f506
Omar Sayed <orhman@hotmail.com>
2024-06-30 14:18:41 +0300

    feat: :sparkles: add jwt creation logic and save it to cookie

--- server/security.utils.ts
+++ server/security.utils.ts
@@ -1,22 +1,28 @@
-
-
-
 import moment = require("moment");
-const util = require('util');
-const crypto = require('crypto');
-import * as jwt from 'jsonwebtoken';
+const util = require("util");
+const crypto = require("crypto");
+import * as jwt from "jsonwebtoken";
 import * as fs from "fs";
-
-
 
 export const randomBytes = util.promisify(crypto.randomBytes);
 
+const RSA_PRIVATE_KEY = fs.readFileSync("./demos/private.key");
 
+const RSA_PUBLIC_KEY = fs.readFileSync("./demos/public.key");
 
-const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
-
-const RSA_PUBLIC_KEY = fs.readFileSync('./demos/public.key');
+export const signJwt = util.promisify(jwt.sign);
 
 const SESSION_DURATION = 240;
 
+const ALGORITHM = "RS256";
 
+const DURATION = 120;
+
+export const createSessionToken = async (userId: string) => {
+  const payload = {};
+  return signJwt(payload, RSA_PRIVATE_KEY, {
+    algorithm: ALGORITHM,
+    expiresIn: DURATION,
+    subject: userId,
+  });
+};
